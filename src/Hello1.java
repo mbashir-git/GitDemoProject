76e6c75c7c4ca703982895515692c1ca534b90ea
mbashir1 <mbashir1@B69285-LAPTOP.ad.douglascollege.ca>
2020-09-25 08:45:52 -0700

    second commit....

--- src/Hello1.java
+++ src/Hello1.java
@@ -4,6 +4,7 @@ public class Hello1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("This is hello1 class in git demo project");
+		System.out.println("make some changes");
 	}
 
 }
