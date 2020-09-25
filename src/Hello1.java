4d961918f66fd152d8dd295b808cd83b029e4962
mbashir1 <mbashir1@B69285-LAPTOP.ad.douglascollege.ca>
2020-09-25 08:29:25 -0700

    First commit

---
+++ src/Hello1.java
@@ -0,0 +1,9 @@
+
+public class Hello1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is hello1 class in git demo project");
+	}
+
+}
